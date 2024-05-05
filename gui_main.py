6c3acf4eb0b73b3d66c98163ac8c9a3cce40efb4
Jonathan Czepa <jonathan.czepa@gmail.com>
2024-05-04 19:59:25 -0500

    sometimes I cry when I'm alone

--- gui_main.py
+++ gui_main.py
@@ -8,26 +8,50 @@ TO-DO:
 4) Add some input limitations
 5) Add seperators  
 6) CSV support
-7) Get Treeview frame working "I can do this one"
-8) AAAAAHHHH FUCK
+7) AAAAAHHHH FUCK
 """
 
 import tkinter as tk
 from tkinter import ttk
 
-
-
-
-
+"""Class that allows for editing treeview stuff, need to fix it from bugging out in fullscreen or just remove fullscreen entirely"""
 class TreeEdit(ttk.Treeview):
+
+    HORIZONAL_OFFSET = 305
+    VERTICAL_OFFSET = 10
+
     def __init__(self, master, **kw):
         super().__init__(master, **kw)
         self.bind("<Double-1>", self.double_click)
+        self.bind("<<TreeviewSelect>>", self.selected_row)
 
-        
+    def selected_row(self, event):
+        self.bind("<Delete>", self.delete_row)
+        self.bind("<Up>", self.up_select)
+        self.bind("<Down>", self.down_select)
+        self.bind("<Escape>", self.deselect)
+
+    def delete_row(self, event):
+        row = self.selection()[0]
+        self.delete(row)
+
+    def up_select(self, event):
+        rows = self.selection()
+        for row in rows:
+            self.move(row, self.parent(row), self.index(row))
+
+    def down_select(self, event):
+        rows = self.selection()
+        for row in rows:
+            self.move(row, self.parent(row), self.index(row))
+
+    def deselect(self, event):
+        if len(self.selection()) > 0:
+            self.selection_remove(self.selection()[0])
+
+
 
     def double_click(self, event):
-        
         try:
             region = self.identify_region(event.x, event.y)
             
@@ -35,26 +59,52 @@ class TreeEdit(ttk.Treeview):
                 return
 
             column = self.identify_column(event.x)
-            column_index = int(column[1:]) - 1
+            self.column_index = int(column[1:]) - 1
 
             value = self.item(self.focus())
-            id = self.focus()
+            self.id = self.focus()
             if column == '#0':
                 selected_values = value.get("text") 
             else:
-                selected_values = value.get("values")[column_index]
+                selected_values = value.get("values")[self.column_index]
             
-            cell = self.bbox(id,column)
-            print(cell)
-            entry = ttk.Entry(root, width=cell[2])
-            
+            cell = self.bbox(self.id,column)
+            self.entry = ttk.Entry(root, width=cell[2])
+        
+            self.entry.insert(0, selected_values)
+            self.entry.select_range(0, tk.END)
+            self.entry.focus()
 
+            self.entry.bind("<FocusOut>", self.focus_out)
+            self.entry.bind("<Return>", self.enter)
+            self.entry.bind("<Escape>", self.escape)
             
-
+            self.entry.place(
+                x=cell[0]+ self.HORIZONAL_OFFSET,
+                y=cell[1]+ self.VERTICAL_OFFSET,
+                w=cell[2],
+                h=cell[3]
+            )
+        
         except IndexError:
             return 0
 
-        print(selected_values)
+    def focus_out(self, event):
+        event.widget.destroy()
+
+    def enter(self, event):
+        text = event.widget.get()
+        values_current = self.item(self.id).get("values")
+        values_current[self.column_index] = text
+        self.item(self.id, values=values_current)
+        event.widget.destroy()
+    
+    def escape(self, event):
+        text = event.widget.get()
+        values_current = self.item(self.id).get("values")[self.column_index]=text
+        event.widget.destroy()
+
+
 class Gui_Main:
     def __init__(self, root):
         self.root = root
@@ -99,42 +149,38 @@ class Gui_Main:
         self.status_type.bind("<<ComboboxSelected>>", self.status_widgets)
 
         # Death Saves Checkbutton
-        self.death_saves = ttk.Checkbutton(self.entry_frame, text="Death Saves")
+        self.has_saves = tk.BooleanVar()
+        self.death_saves = ttk.Checkbutton(self.entry_frame, text="Death Saves", variable=self.has_saves)
         self.death_saves.grid(row=7, column=0, sticky="ew")
 
-        # Buttons
-        self.insert_button = ttk.Button(self.entry_frame, text="Insert")
+        # Insert Button
+        self.insert_button = ttk.Button(self.entry_frame, text="Insert", command=self.insert_button)
         self.insert_button.grid(row=9, columnspan=2, column=0, sticky="ew")
 
+        # Roll Button
         self.roll_button = ttk.Button(self.entry_frame, text="ROLL")
         self.roll_button.grid(row=10, columnspan=2, column=0, sticky="ew")
 
         #Tree Table
         table_scroll = ttk.Scrollbar(self.table_frame)
         table_scroll.pack(side="right", fill="y")
-        cols = ("turn", "name", "initiative", "ac", "hp", "damage", "healing", "death_saves")
-        table_view = TreeEdit(self.table_frame, show="headings", yscrollcommand=table_scroll.set, columns=cols, height=13)
-        table_view.column("turn", width=50)
-        table_view.column("name", width=250)
-        table_view.column("initiative", width=100)
-        table_view.column("ac", width=100)
-        table_view.column("hp", width=100)
-        table_view.column("damage", width=100)
-        table_view.column("healing", width=100)
-        table_view.column("death_saves", width=250) 
-        
-        table_view.insert(
-            "",
-            tk.END,
-            text="",
-            values=("8", "18:30")
-        )
 
- 
-        
-        table_view.pack()
-        table_scroll.config(command=table_view.yview)
+        cols = ("turn", "name", "initiative", "ac", "hp", "thp", "damage", "healing", "death_saves")
+        self.table_view = TreeEdit(self.table_frame, show="headings", yscrollcommand=table_scroll.set, columns=cols, height=13)
 
+
+        self.table_view.column("turn", width=50)
+        self.table_view.column("name", width=150)
+        self.table_view.column("initiative", width=100)
+        self.table_view.column("ac", width=70)
+        self.table_view.column("hp", width=70)
+        self.table_view.column("thp", width=70)
+        self.table_view.column("damage", width=70)
+        self.table_view.column("healing", width=70)
+        self.table_view.column("death_saves", width=70) 
+
+        self.table_view.pack()
+        table_scroll.config(command=self.table_view.yview)
 
     def create_entry(self, frame, label_text, initial_text, row):
         label = ttk.Label(frame, text=label_text)
@@ -144,10 +190,6 @@ class Gui_Main:
         entry.bind("<FocusIn>", lambda e, entry=entry: entry.delete(0, 'end'))
         entry.grid(row=row, column=1, sticky="ew")
         return entry
-    """
-    def create_entry_row(self, frame, entry_data, row):
-        entry = ttk.Entry(frame)
-    """
 
     def add_thp_entry(self):
         if self.has_thp.get():
@@ -166,6 +208,39 @@ class Gui_Main:
         elif status == "PC":
             self.death_saves.grid_remove()
 
+
+    def insert_button(self):
+        
+        if self.has_saves.get():
+            death_saves = "3/3"
+        else:
+            death_saves = "N/A"
+
+        creature_values = (
+        "",
+        self.name_entry.get(),
+        "0",
+        self.ac_entry.get(),
+        self.hp_entry.get(),
+        self.thp_entry.get(),
+        "0",
+        "0",
+        death_saves
+        )
+
+        self.insert_row(creature_values)
+
+
+    def insert_row(self, creature_values):
+        self.table_view.insert(
+            "",
+            tk.END,
+            text="",
+            values = creature_values
+        )
+
+
+
     """
     callback to ensure certain entries are integers only.
     not in use right now 
@@ -175,9 +250,6 @@ class Gui_Main:
             return True
         else:
             return False
-        
-    def get_table_frame(self):
-        return self.table_frame
 
 if __name__ == "__main__":
     root = tk.Tk()
